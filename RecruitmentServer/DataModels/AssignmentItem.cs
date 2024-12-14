5feda691901bd1bdaa9ed36b8e72c6d9561f3cc4
Bohdan Kalnytskyi <bogdan201375@gmail.com>
2024-12-14 17:14:43 +0200

    Renamed client and server projects.

---
+++ RecruitmentServer/DataModels/AssignmentItem.cs
@@ -0,0 +1,16 @@
+﻿namespace RecruitmentServer.DataModels
+{
+    internal class AssignmentItem
+    {// Клас для вирішення задачі про призначення
+        internal int IdVacancy { get; private set; }// Код вакансії
+        internal int IdCandidate { get; private set; }// Код кандидата
+        internal int Scores { get; private set; }// Кількість балів
+
+        public AssignmentItem(int idVacancy, int idCandidate, int scores)
+        {// Конструктор
+            IdVacancy = idVacancy;
+            IdCandidate = idCandidate;
+            Scores = scores;
+        }
+    }
+}
